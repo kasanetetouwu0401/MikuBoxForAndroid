b46f4775da180a75dbad2fae6ae1e425ac2cd1e9
Hatsune Miku <146099520+HatsuneMikuUwU@users.noreply.github.com>
2025-10-05 03:17:33 +0700

    Update CircleImageView.java

--- app/src/main/java/com/neko/circleimageview/CircleImageView.java
+++ app/src/main/java/com/neko/circleimageview/CircleImageView.java
@@ -44,7 +44,7 @@ import androidx.annotation.ColorRes;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
-import io.nekohasekai.sagernet.R
+import io.nekohasekai.sagernet.R;
 
 @SuppressWarnings("UnusedDeclaration")
 public class CircleImageView extends AppCompatImageView {
