b85de43c614f0b41f7c1eb16767e84776a0534b5
Hatsune Miku <146099520+HatsuneMikuUwU@users.noreply.github.com>
2025-10-23 07:59:47 +0700

    add RandomText

--- app/src/main/java/com/neko/marquee/text/RandomText.java
+++ app/src/main/java/com/neko/marquee/text/RandomText.java
@@ -5,30 +5,20 @@ import android.content.res.Resources;
 import android.os.Handler;
 import android.util.AttributeSet;
 import androidx.appcompat.widget.AppCompatTextView;
-import com.neko.marquee.text.AutoMarqueeTextView;
-import android.widget.FrameLayout;
 
 import java.util.Random;
 
 public class RandomText extends AppCompatTextView implements Runnable {
 
-    private final AutoMarqueeTextView marqueeTextView;
     private final Handler handler;
     private final boolean runnable;
 
     public RandomText(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        // Create an instance of AutoMarqueeTextView (composition)
-        marqueeTextView = new AutoMarqueeTextView(context, attrs);
-        addView(marqueeTextView, new LayoutParams(
-                LayoutParams.MATCH_PARENT,
-                LayoutParams.WRAP_CONTENT
-        ));
-
         handler = new Handler();
 
-        // Get "uwu_runnable" attribute from XML (default = true)
+        // Ambil atribut "uwu_runnable" dari XML (default = true)
         runnable = attrs.getAttributeBooleanValue(null, "uwu_runnable", true);
 
         if (runnable) {
@@ -41,7 +31,7 @@ public class RandomText extends AppCompatTextView implements Runnable {
     @Override
     public void run() {
         setRandomText();
-        // Rerun every 3000 ms (0xbb8 in hexadecimal = 3000)
+        // Jalankan ulang setiap 3000 ms (0xbb8 dalam heksadesimal = 3000)
         handler.postDelayed(this, 3000);
     }
 
@@ -50,10 +40,10 @@ public class RandomText extends AppCompatTextView implements Runnable {
         Context context = getContext();
         String packageName = context.getPackageName();
 
-        // Get an array resource named "uwu_random_text"
+        // Ambil resource array bernama "uwu_random_text"
         int id = res.getIdentifier("array/uwu_random_text", "array", packageName);
         if (id == 0) {
-            return; // If not found, exit
+            return; // Jika tidak ditemukan, keluar
         }
 
         String[] texts = res.getStringArray(id);
@@ -63,15 +53,6 @@ public class RandomText extends AppCompatTextView implements Runnable {
 
         Random random = new Random();
         int index = random.nextInt(texts.length);
-        marqueeTextView.setText(texts[index]);
-    }
-
-    // Additional methods to be able to use it like a normal TextView
-    public void setText(CharSequence text) {
-        marqueeTextView.setText(text);
-    }
-
-    public CharSequence getText() {
-        return marqueeTextView.getText();
+        setText(texts[index]);
     }
 }
