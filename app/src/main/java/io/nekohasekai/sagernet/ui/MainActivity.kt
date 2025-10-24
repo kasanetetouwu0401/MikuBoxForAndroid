20169349c70b02cf2afc9b04fc9e18a65776a7d8
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2025-10-25 02:41:14 +0700

    Update MainActivity.kt

--- app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
@@ -55,6 +55,7 @@ import android.view.View
 import androidx.core.view.ViewCompat
 import io.nekohasekai.sagernet.widget.FabStyle
 import com.airbnb.lottie.LottieAnimationView
+import android.animation.Animator
 
 class MainActivity : ThemedActivity(),
     SagerConnection.Callback,
@@ -123,15 +124,18 @@ class MainActivity : ThemedActivity(),
         lottieView.visibility = View.VISIBLE
         lottieView.playAnimation()
 
-        lottieView.addAnimatorListener(
-            onEnd = {
+        lottieView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {}
+            override fun onAnimationEnd(animation: Animator?) {
                 lottieView.animate()
                     .alpha(0f)
                     .setDuration(900)
                     .withEndAction { lottieView.visibility = View.GONE }
                     .start()
             }
-        )
+            override fun onAnimationCancel(animation: Animator?) {}
+            override fun onAnimationRepeat(animation: Animator?) {}
+        })
         
         changeState(BaseService.State.Idle)
         connection.connect(this, this)
