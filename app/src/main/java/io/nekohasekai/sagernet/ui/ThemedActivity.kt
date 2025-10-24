e9200602b3dfb892a6de6bc21b846fcda5f35e61
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2025-10-25 00:32:16 +0700

    Update ThemedActivity.kt

--- app/src/main/java/io/nekohasekai/sagernet/ui/ThemedActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/ThemedActivity.kt
@@ -10,10 +10,15 @@ import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.snackbar.Snackbar
 import io.nekohasekai.sagernet.R
 import io.nekohasekai.sagernet.utils.Theme
+import io.nekohasekai.sagernet.utils.blurBackground
+import io.nekohasekai.sagernet.utils.clearBlur
+import jp.wasabeef.blurry.Blurry
 
 abstract class ThemedActivity : AppCompatActivity {
     constructor() : super()
@@ -35,30 +40,28 @@ abstract class ThemedActivity : AppCompatActivity {
 
         uiMode = resources.configuration.uiMode
 
+        // Atur padding atas untuk status bar di Android 14+
         if (Build.VERSION.SDK_INT >= 35) {
             ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { _, insets ->
                 val top = insets.getInsets(WindowInsetsCompat.Type.systemBars()).top
                 findViewById<AppBarLayout>(R.id.appbar)?.apply {
                     updatePadding(top = top)
-//                Logs.w("appbar $top")
                 }
-//            findViewById<NavigationView>(R.id.nav_view)?.apply {
-//                updatePadding(top = top)
-//            }
                 insets
             }
         }
+
+        // Aktifkan blur otomatis untuk semua dialog di Activity ini
+        enableGlobalDialogBlur()
     }
 
     override fun setTheme(resId: Int) {
         super.setTheme(resId)
-
         themeResId = resId
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-
         if (newConfig.uiMode != uiMode) {
             uiMode = newConfig.uiMode
             ActivityCompat.recreate(this)
@@ -74,4 +77,25 @@ abstract class ThemedActivity : AppCompatActivity {
 
     internal open fun snackbarInternal(text: CharSequence): Snackbar = throw NotImplementedError()
 
+    // Tambahkan fungsi global blur listener
+    private fun enableGlobalDialogBlur() {
+        supportFragmentManager.registerFragmentLifecycleCallbacks(
+            object : FragmentManager.FragmentLifecycleCallbacks() {
+                override fun onFragmentViewCreated(
+                    fm: FragmentManager,
+                    f: androidx.fragment.app.Fragment,
+                    v: android.view.View,
+                    savedInstanceState: Bundle?
+                ) {
+                    if (f is DialogFragment) {
+                        blurBackground()
+                        f.dialog?.setOnDismissListener {
+                            clearBlur()
+                        }
+                    }
+                }
+            },
+            true
+        )
+    }
 }
