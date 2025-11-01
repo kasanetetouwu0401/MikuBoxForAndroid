ef4160d84f8c6dbd69fb3f5338d71d0a8151c557
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2025-11-02 00:42:47 +0700

    Update GroupFragment.kt

--- app/src/main/java/io/nekohasekai/sagernet/ui/GroupFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/GroupFragment.kt
@@ -14,6 +14,7 @@ import androidx.core.view.*
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import io.nekohasekai.sagernet.GroupType
 import io.nekohasekai.sagernet.R
